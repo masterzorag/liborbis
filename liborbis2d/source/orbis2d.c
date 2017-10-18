311b586d8b7b3c79e7792e403c91b4d590712513
masterzorag <masterzorag@gmail.com>
2017-10-18 16:28:54 +0200

    Update orbis2dDrawPixelColor

--- liborbis2d/source/orbis2d.c
+++ liborbis2d/source/orbis2d.c
@@ -169,14 +169,13 @@ void orbis2dStartDrawing()
 }
 void orbis2dDrawPixelColor(int x, int y, uint32_t pixelColor)
 {
-	int color;
-	int pixel = (y * orbconf->pitch) + x;
+	if((x > -1 && x < ATTR_WIDTH)
+	&& (y > -1 && y < ATTR_HEIGHT))
+	{
+		int pixel = (y * orbconf->pitch) + x;
 
-	color=pixelColor;
-
-	
-	((uint32_t *)orbconf->surfaceAddr[orbconf->currentBuffer])[pixel]=color;
-
+		((uint32_t *)orbconf->surfaceAddr[orbconf->currentBuffer])[pixel] = pixelColor;
+	}
 }
 void orbis2dDrawRectColor(int x, int w, int y, int h, uint32_t color)
 {
