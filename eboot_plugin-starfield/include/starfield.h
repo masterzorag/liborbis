5d68e4dfd4ee01c3b75d36c8dc8c53033660e1ac
masterzorag <masterzorag@gmail.com>
2017-11-14 11:27:55 +0100

    Remove macros, are part of liborbisXmbFont yet

--- eboot_plugin-starfield/include/starfield.h
+++ eboot_plugin-starfield/include/starfield.h
@@ -12,16 +12,6 @@ typedef struct _STAR{
 
 #define NUMBER_OF_STARS 256*4     // max 2^16 for uint16_t
 
-// compose ARGB color by components
-#define ARGB(a, r, g, b) ( \
-          (((a) &0xFF) <<24) | (((r) &0xFF) <<16) | \
-          (((g) &0xFF) << 8) | (((b) &0xFF) << 0))
-
-// extract single component form ARGB color
-#define GET_A(color) ((color >>24) &0xFF)
-#define GET_R(color) ((color >>16) &0xFF)
-#define GET_G(color) ((color >> 8) &0xFF)
-#define GET_B(color) ((color >> 0) &0xFF)
 
 void init_Star(STAR *star, const uint16_t i);
 void init_Starfield(void);
